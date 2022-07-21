6de6f2515100905bc082e2e5905b975f05631363
MerveMay <mmervecelik36@gmail.com>
2022-07-21 15:08:04 -0400

    class06

---
+++ Class06/HandlingAlerts.java
@@ -0,0 +1,43 @@
+package Class06;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingAlerts {
+    public static String url = "http://www.uitestpractice.com/Students/Switchto";
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        WebElement alertButton= driver.findElement(By.id("alert"));
+        alertButton.click();
+        Thread.sleep(1000);
+        Alert alert= driver.switchTo().alert();
+        alert.accept();
+        WebElement simpleAlertButton = driver.findElement(By.id("alert"));
+        simpleAlertButton.click();
+        Thread.sleep(2000);
+        Alert simpleAlert = driver.switchTo().alert(); // switch focus to alert
+        simpleAlert.accept();
+
+        WebElement confirmAlertButton = driver.findElement(By.id("confirm"));
+        confirmAlertButton.click();
+        Thread.sleep(2000);
+
+        Alert confirmAlert = driver.switchTo().alert();
+        String confirmAlertText = confirmAlert.getText();
+        System.out.println(confirmAlertText);
+        confirmAlert.dismiss();
+
+        WebElement promptAlertButton = driver.findElement(By.id("prompt"));
+        promptAlertButton.click();
+        Thread.sleep(2000);
+        Alert promptAlert = driver.switchTo().alert();
+        promptAlert.sendKeys("Dont replace my Chromedriver!!!");
+        promptAlert.accept();
+    }
+    }
